89d77e88834e00ee5042b81d026a8be395cf6c5b
c-argus <>
2023-07-30 10:08:36 +0000

    Define necessary models in Django

---
+++ stocklist/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+# Create two models, one for Item and one for Quantity
+# Model Item represents individual items
+# Model Quantity represents the quantity of each item
+
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Quantity(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} units of {self.item}"
