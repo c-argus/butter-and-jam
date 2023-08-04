ae20477ae2e1367bf17e9d272b5a7b3b615b1815
c-argus <>
2023-08-04 12:01:33 +0000

    Fix error, removed references to the Quantity model

--- stocklist/admin.py
+++ stocklist/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Item, Quantity
+from .models import Item
 
 admin.site.register(Item)
-admin.site.register(Quantity)
--- stocklist/models.py
+++ stocklist/models.py
@@ -2,23 +2,13 @@ from django.db import models
 
 # Create your models here.
 # Create two models, one for Item and one for Quantity
-# Model Item represents individual items
-# Model Quantity represents the quantity of each item
-
+# Model Item has the quantity of each item.
+# Quantity is of type PositiveIntegerField to ensure the quantity value won't be negative
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    objects = models.Manager()
-
-    def __str__(self):
-        return self.name
-
-
-class Quantity(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.quantity} units of {self.item}"
+        return f"{self.name} (Price: {self.price}, Quantity: {self.quantity})"
