505307fbfc2149d8e858168274a6e03efc2433ec
c-argus <>
2023-08-04 14:59:26 +0000

    Add reorder model

--- stocklist/models.py
+++ stocklist/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Model Item has the quantity of each item.
 # Quantity is of type PositiveIntegerField to ensure the quantity value won't be negative
 
+
 class Item(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -12,3 +13,12 @@ class Item(models.Model):
 
     def __str__(self):
         return f"{self.name} (Price: {self.price}, Quantity: {self.quantity})"
+
+    def needs_reorder(self):
+        reorder_threshold = 5
+
+        # Check if the item's quantity is below the reorder threshold
+        if self.quantity < reorder_threshold:
+            return True
+        else:
+            return: False
--- stocklist/views.py
+++ stocklist/views.py
@@ -19,5 +19,6 @@ def item_list(request):
 
 
 def item_detail(request, item_id):
-    item = get_object_or_404(Item, id=item_id)
-    return render(request, 'item_detail.html', {'item': item})
+    item = Item.objects.get(id=item_id)
+    needs_reorder = item.needs_reorder()  # Call the needs_reorder method
+    return render(request, 'item_detail.html', {'item': item, 'needs_reorder': needs_reorder})
