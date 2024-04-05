15c223671e23792ca5658f13e25de7a1c2f3a534
c-argus <camila.argus88@gmail.com>
2024-04-05 12:52:06 +0000

    Changed version for psycopg and comment out database in settings

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'django_bandj.wsgi.application'
 #    }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
