dd1283e133395ea7a3d21bf3ebbceac1fbd97747
c-argus <camila.argus88@gmail.com>
2024-04-06 11:40:07 +0000

    Fix database configuration issue in settings.py

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'django_bandj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
