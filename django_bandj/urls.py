3518c0531e9f9f8c2a578c99e799535710ccd359
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-07-31 20:28:14 +0000

    Update registration form with Bootstrap validation

--- django_bandj/urls.py
+++ django_bandj/urls.py
@@ -33,6 +33,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='stoklist/logout.html'), name='logout'),
     path('login/', views.custom_login, name='login'),
     path('notifications/mark_as_read/<int:notification_id>/', views.mark_notification_as_read, name='mark_notification_as_read'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
