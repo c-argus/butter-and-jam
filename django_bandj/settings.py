e40bc9c93b9bb059e0b409f1d886cd17ed630323
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-02 13:51:05 +0000

    security key test2

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'sinixtro@14*17')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'True') == 'True'
