c99443afb5670be0835941ea1bb0db0a01cc2355
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-02 13:22:02 +0000

    Dangerous heroku test

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -156,3 +156,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
