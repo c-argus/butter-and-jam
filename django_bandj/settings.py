711f2271e70b74055226f4c6101109e294e9a3c1
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-05 19:00:32 +0000

    crfs trusted origins add

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -163,4 +163,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
