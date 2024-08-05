34f2d36ee0243d7b6d7129db49cc476232cdeacc
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-05 16:18:49 +0000

    dangerous message error fix test

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -25,7 +25,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'sinixtro@14*17')
+# SECRET_KEY = os.getenv('SECRET_KEY', 'sinixtro@14*17')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for Django application")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', 'True') == 'True'
