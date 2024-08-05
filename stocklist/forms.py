b6663e4df0e74b123744c85b1b478c42037d28d4
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-05 16:35:13 +0000

    heroku dangerous message fix error teste2

--- stocklist/forms.py
+++ stocklist/forms.py
@@ -32,6 +32,19 @@ class UserRegistrationForm(forms.ModelForm):
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
 
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
         if password != confirm_password:
             raise forms.ValidationError('Passwords do not match')
 
