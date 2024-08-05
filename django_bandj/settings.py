51f09f21bde74bcc13bdee763e196a0a9663a7e9
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-05 19:13:20 +0000

    crsf key

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -165,6 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "https://8000-cargus-butterandjam-2izmz6gbrk3.ws.codeinstitute-ide.net",
+    "https://cabare-stocklist-bandj-64bab33316f2.herokuapp.com/",
 ]
 
