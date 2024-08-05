67f8fd5d63733006b69550df8240191231395a43
Camila de Figueiredo Argus <camila.argus88@gmail.com>
2024-08-05 19:22:54 +0000

    crsf

--- django_bandj/settings.py
+++ django_bandj/settings.py
@@ -165,6 +165,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "http://127.0.0.1:8000",
+    "https://*.gitpod.io"
 ]
-
